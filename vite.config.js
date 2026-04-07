17c0ee27cb708f61aaaf2a8cdba2bc41437210f9
kpp-sketch <katerina.pospichalovapavlov@prf.cuni.cz>
2026-04-07 11:51:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cios_website', // Replace with your actual repository name
+  base: '/', // Replace with your actual repository name
 })
