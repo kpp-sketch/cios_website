3e3afacc2d13fbaf48da9c35d243396a438688e3
kpp-sketch <katerina.pospichalovapavlov@prf.cuni.cz>
2026-04-21 14:35:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -298,6 +298,7 @@ export default function App() {
                 </div>
               ))}
             </div>
+           </div> {/* TUTO ZÁVORKU ZDE PŘIDEJTE */}
         );
       default: return null;
     }
