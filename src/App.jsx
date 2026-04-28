8073bb36f4bf9074c1fa67ee066b18464bff5194
kpp-sketch <katerina.pospichalovapavlov@prf.cuni.cz>
2026-04-28 10:50:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -172,7 +172,7 @@ const isabMembers = [
 
       try {
 
-        const teamRes = await fetch('/team.xlsx');
+        const teamRes = await fetch('/team.xlsx?t=' + new Date().getTime());
 
         if (teamRes.ok) {
 
